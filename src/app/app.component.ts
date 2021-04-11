6f620370071c50d43c7e4c65844261941893ab03
Adam Ferchichi <Adam.Ferchichi@contracted.pmi.com>
2021-04-11 12:56:26 +0200

    birthday edition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {trigger, transition, useAnimation} from "@angular/animations";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [
-    trigger('rotateCubeToLeft', [ transition('* => *', useAnimation(moveFromLeftFade))])
+    trigger('rotateFlipToTop', [ transition('* => *', useAnimation(rotateFlipToTop))])
   ]
 })
 export class AppComponent {
