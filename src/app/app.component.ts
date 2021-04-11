53c125d276df1e2961a347865013e3b877e595f0
Adam Ferchichi <Adam.Ferchichi@contracted.pmi.com>
2021-04-11 12:58:29 +0200

    birthday edition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import {moveFromLeftFade} from "ngx-router-animations";
+import {rotateCarouselToRight} from "ngx-router-animations";
 import {trigger, transition, useAnimation} from "@angular/animations";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [
-    trigger('rotateFlipToTop', [ transition('* => *', useAnimation(rotateFlipToTop))])
+    trigger('rotateFlipToTop', [ transition('* => *', useAnimation(rotateCarouselToRight))])
   ]
 })
 export class AppComponent {
